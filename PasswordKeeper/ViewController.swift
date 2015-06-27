e62d2ec6545d7c9dc764eebe874183d15dc4fac7
iosnerds <ss@ssdeMacBook-Pro.local>
2015-06-27 23:45:40 +0800

    Initial Commit

---
+++ PasswordKeeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PasswordKeeper
+//
+//  Created by ss on 15/6/27.
+//  Copyright (c) 2015年 ss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
