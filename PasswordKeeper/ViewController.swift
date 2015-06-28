0ab764aa2cfb07001568b9b1e5a2226fca8be9e7
zl <ZL_0401@yeah.net>
2015-06-28 13:44:14 +0800

    增加侨界文件

--- PasswordKeeper/ViewController.swift
+++ PasswordKeeper/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -18,6 +19,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        let a = MYIntroductionPanel()
     }
 
 
