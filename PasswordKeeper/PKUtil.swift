1640d50965540adcd184be9606b83bd93cabad41
zl <ZL_0401@yeah.net>
2015-07-14 23:13:05 +0800

    大界面

---
+++ PasswordKeeper/PKUtil.swift
@@ -0,0 +1,25 @@
+//
+//  PKUtil.swift
+//  PasswordKeeper
+//
+//  Created by 龚莎 on 7/14/15.
+//  Copyright (c) 2015 ss. All rights reserved.
+//
+
+import UIKit
+
+class PKUtil: NSObject {
+   
+    /// 根据资源名找到图片
+    /// TODO:ios9访问tag资源
+    static func fetchIcon(sourceName : String) -> Image {
+        
+    }
+    
+    /// 根据图片得到图片的背景色
+    /// TODO:渐变色增加中间是白色
+    static func fetchIconBackgourndColor(img : Image) -> Color {
+        
+    }
+
+}
